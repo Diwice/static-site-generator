f7511917f32d769a77fd64cd61f5c0dcd5873f86
Diwice <coldwice@gmail.com>
2025-09-27 17:47:57 +0300

    Unfinished CH3L5

--- src/inlineparser.py
+++ src/inlineparser.py
@@ -26,3 +26,22 @@ def extract_markdown_images(node) :
 	res_matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", node.text)
 	return res_matches if res_matches else None
 
+def split_nodes_image(old_nodes) :
+	new_nodes = []
+
+	for node in old_nodes :
+		if node.text_type is not TextType.TEXT :
+			new_nodes.append(node)
+			continue
+
+		extract_images = extract_markdown_images(node)
+		for image in extract_images :
+			new_nodes.extend([LeafNode(tag=None, value=node.text[:node.text.index("!["+image[0]):]),LeafNode(tag=TextType.IMAGE, value="", props={"alt":image[0],"":image[1]}),LeafNode(tag=None, value=node.text[node.text.index(image[1])+len(image[1])+1::])])
+
+	return new_nodes
+
+#def split_nodes_link(node) :
+#	...
+
+some_node = [TextNode("This node contains ![google lol](https://google.com/) 1 image", TextType.TEXT)]
+print(split_nodes_image(some_node))
