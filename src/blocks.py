009b7b3198ff2697a49fb3a430cb443123221666
Diwice <coldwice@gmail.com>
2025-09-28 19:51:29 +0300

    CH4L2

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,32 @@
+import re
+from enum import Enum
+
+class BlockType(Enum) :
+	paragraph = None
+	heading = None
+	code = None
+	quote = None
+	unordered_list = None
+	ordered_list = None
+
 def markdown_to_blocks(markdown) :
 	res = markdown.split("\n\n")
+
 	return [i.strip() for i in res if i]
+
+def block_to_block_type(block) :
+	ord_list_sub = []
+
+	if re.match(r"#{1,6}\s", block) :
+		return BlockType.heading
+	elif re.match(r"`{3}.+`{3}", block) :
+		return BlockType.code
+	elif all(i[0] == ">" for i in block.split("\n")) :
+		return BlockType.quote
+	elif all(i[0:2] == "- " for i in block.split("\n")) :
+		return BlockType.unordered_list
+	elif all(re.match(r"^\d+\.\s", i) and not(ord_list_sub.append(int(i[0]))) for i in block.split("\n")) :
+		if all(ord_list_sub[i] == ord_list_sub[i+1] - 1 for i in range(len(ord_list_sub)-1)) :
+			return BlockType.ordered_list
+
+	return BlockType.paragraph
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -18,3 +18,43 @@ This is the same paragraph on a new line
 """
 		blocks = markdown_to_blocks(md)
 		self.assertEqual(blocks, ["This is **bolded** paragraph", "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line", "- This is a list\n- with items"])
+
+	def test_paragraph_type(self) :
+		block = "Some plain text"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.paragraph)
+
+	def test_heading_type(self) :
+		block = "### Some text"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.heading)
+
+	def test_code_type(self) :
+		block = "```Some code text```"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.code)
+
+	def test_quote_type(self) :
+		block = ">This block\n>is a quote block"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.quote)
+
+	def test_unordered_list_type(self) :
+		block = "- unordered\n- list"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.unordered_list)
+
+	def test_ordered_list_type(self) :
+		block = "1. This\n2. is\n3. ordered\n4. list"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.ordered_list)
+
+	def test_fake_ordered_list_type(self) :
+		block = "1. some\n45. text"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.paragraph)
+
+	def test_mixed_list_type(self) :
+		block = "- mixed\n- list\n3. here"
+		block_type = block_to_block_type(block)
+		self.assertEqual(block_type, BlockType.paragraph)
