03326a510464679d00d4d8e60fac2071732d2420
Diwice <coldwice@gmail.com>
2025-10-07 15:24:48 +0300

    CH5L4

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main() :
     os.mkdir(os.path.abspath("./public"))
 
     copy_static()
-    generate_page("content/index.md", "template.html", "public/index.html")    
+    generate_pages_recursive("content", "template.html", "public") 
 
 if __name__ == "__main__" :
     main()
--- src/page_generator.py
+++ src/page_generator.py
@@ -4,7 +4,9 @@ from blocks import markdown_to_html_node, extract_title
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from '{from_path}' to '{dest_path}' using '{template_path}'")
 
-    from_path, template_path, dest_path = os.path.abspath("./"+from_path), os.path.abspath("./"+template_path), os.path.abspath("./"+dest_path)
+    from_path = os.path.abspath("./"+from_path) if not(os.path.isabs(from_path)) else from_path
+    template_path = os.path.abspath("./"+template_path) if not(os.path.isabs(template_path)) else template_path
+    dest_path = os.path.abspath("./"+dest_path) if not(os.path.isabs(dest_path)) else dest_path
 
     with open(from_path, "r") as f_file:
         md = f_file.read()
@@ -21,3 +23,15 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, "w") as new_file:
         new_file.write(res)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dir_path_content = os.path.abspath("./"+dir_path_content) if not(os.path.isabs(dir_path_content)) else dir_path_content
+    dest_dir_path = os.path.abspath("./"+dest_dir_path) if not(os.path.isabs(dest_dir_path)) else dest_dir_path
+    sub_items = [os.path.join(dir_path_content, i) for i in os.listdir(dir_path_content)]
+    for item in sub_items :
+        if not(os.path.isfile(item)) :
+            generate_pages_recursive(item, template_path, dest_dir_path+item.replace(dir_path_content, "", 1))
+            continue
+
+        if item.split("/")[-1].split(".")[-1] == "md" :
+            generate_page(item, template_path, os.path.join(dest_dir_path, item.split("/")[-1].split(".")[0]+".html"))
